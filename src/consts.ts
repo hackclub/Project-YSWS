5d03742ffe9cecaf475ccf3f84c8b6b0b0f51a98
dhyan <87286496+Bluelightning26@users.noreply.github.com>
2026-02-16 23:19:38 -0500

    fix for unfurling links

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Project YSWS';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Create your own custom workshop, get funding from Hack Club to make it a reality';
 export const REPOSITORY_URL = 'https://github.com/bluelightning26/cat-boogie'
